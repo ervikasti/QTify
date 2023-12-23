358bd8ef76d0501fa0ed29d227038547e283a0cc
Vikas Tiwari <30732434+ervikasti@users.noreply.github.com>
2023-12-24 01:33:31 +0530

    navbar gui completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,10 +2,12 @@ import React from "react";
 import FeedbackBtn from "../FeedbackBtn/FeedbackBtn";
 import styles from './Navbar.module.css';
 import { Search } from "../Search/Search";
+import { Logo } from "../Logo/Logo";
 
 function Navbar(){
     return(
         <div className={styles.navbar}>
+            <Logo />
             <Search placeholder={'Search a album of your choice'}/>
             <FeedbackBtn>Give Feedback</FeedbackBtn>
         </div>
